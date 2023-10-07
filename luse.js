f56b8ef8eb998e4805dbfe37833d748d6dacbdc6
LemanEskerova12 <eskerevaleman559@gmail.com>
2023-10-07 14:37:18 +0400

    first commit

---
+++ luse.js
@@ -0,0 +1,17 @@
+function myFunction1(){
+    let a = +prompt('a terefini yazin');
+let h = +prompt('h terefini yazin');
+let S = (a * h) / 2
+let netice = S;
+
+document.getElementById('ziko').innerHTML = netice;
+}
+
+function myFunction(){
+    let a1 = prompt('a terefini yazin');
+   let h1 = prompt('h terefini yazin');
+    let s = (a1*h1)
+    let cavab = s;
+
+document.getElementById('luse').innerHTML = cavab;
+}
